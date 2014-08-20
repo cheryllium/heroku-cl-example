bbaf3704514d45e4306368973ea9f1d2641ac7bc
Brandon Bohrer <bbohrer@andrew.cmu.edu>
2014-08-19 21:57:05 -0400

    try to fix order of some stuff

--- heroku-setup.lisp
+++ heroku-setup.lisp
@@ -2,15 +2,16 @@
 
 (print ">>> Building system....")
 
-(load (make-pathname :directory *build-dir* :defaults "example.asd"))
-
-(ql:quickload :example)
-
 (asdf:run-shell-command
  (format nil "cp -r ~Apublic ~A"
          (namestring (asdf:component-pathname (asdf:find-system :example)))
          (namestring (make-pathname :directory (append cl-user::*build-dir* '("public"))))
          ))
+
+(load (make-pathname :directory *build-dir* :defaults "example.asd"))
+
+(ql:quickload :example)
+
 
 ;;; Copy wuwei public files to build
 (wu:heroku-install-wupub-files)
